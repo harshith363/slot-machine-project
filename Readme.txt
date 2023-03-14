1a156d69c56c637f733a26c8b13a3d75b7de4ab8
harshit363 <harshithvasanthkumar@gmail.com>
2023-03-15 00:19:55 +0530

    first commit

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+This is a small python project that I did by coding along with Tech With Tim:
+        https://www.youtube.com/watch?v=th4OBktqK1I
---
+++ main.py
@@ -0,0 +1,116 @@
+import random
+
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbols_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(column) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        print()
+
+
+def deposit():
+    while True:
+        amount = input("How much money would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater then 0.")
+        else:
+            print("Please enter a number")
+
+    return amount
+
+
+def get_number_of_lines():
+    while True:
+        lines = input(
+            "Enter the number of lines you want to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter the number of lines between (1-" +
+                      str(MAX_LINES) + ")? ")
+        else:
+            print("Please enter a number")
+
+    return lines
+
+
+def get_bet():
+    while True:
+        amount = input(
+            "how much money would you like to bet on each line: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} and ${MAX_BET}.")
+        else:
+            print("Please enter a number")
+
+    return amount
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet > balance:
+            print(
+                f"You do not have enough balance for the bet. You current balance is ${balance}")
+        else:
+            break
+
+    print(
+        f"You are betting  ${bet} on {lines} line. Yout total bet is ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbols_count)
+    print_slot_machine(slots)
+
+
+main()
